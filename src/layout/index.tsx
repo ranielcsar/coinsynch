a3f57840fd480f60826115623154b280b5a3db92
ranoob <whyfck@gmail.com>
2023-07-24 20:55:14 -0300

    feat: main layout

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,15 +1,17 @@
 import { PropsWithChildren } from 'react'
-import { Roboto } from 'next/font/google'
 
+import { Roboto } from 'next/font/google'
 const roboto = Roboto({
   subsets: ['latin'],
   variable: '--roboto',
-  weight: ['500', '700'],
+  weight: ['400', '500', '700'],
 })
 
 export function Layout({ children }: PropsWithChildren<any>) {
   return (
-    <div className={`text-default_text ${roboto.className}`}>
+    <div
+      className={`text-default_text ${roboto.className} m-auto flex min-h-screen w-full flex-col bg-white`}
+    >
       <>{children}</>
     </div>
   )
