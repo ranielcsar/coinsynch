616a86f15ce33e78648323e2a43542cc4c570658
ranoob <whyfck@gmail.com>
2023-07-24 20:54:13 -0300

    feat: icons

---
+++ src/assets/icons/menu.tsx
@@ -0,0 +1,42 @@
+export function MenuIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="30"
+      height="30"
+      viewBox="0 0 24 24"
+      fill="none"
+    >
+      <circle
+        cx="12"
+        cy="12"
+        r="9"
+        stroke="#8C8A97"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M7.25 12H16.75"
+        stroke="#8C8A97"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M7.25 15.25H16.75"
+        stroke="#8C8A97"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M7.25 8.75H16.75"
+        stroke="#8C8A97"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  )
+}
