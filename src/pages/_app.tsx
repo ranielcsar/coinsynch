494fcbfe7a36c74cc22251c3b41618aed7996780
ranoob <whyfck@gmail.com>
2023-07-24 20:55:45 -0300

    lint: fixes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
-import { Layout } from '@/layout'
-import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+
+import { Layout } from '@/layout'
+
+import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
