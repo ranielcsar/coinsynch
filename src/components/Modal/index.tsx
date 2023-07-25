c0e4a8fef8b9e5ce5596b3226c64ce1f601f6754
ranoob <whyfck@gmail.com>
2023-07-25 19:51:25 -0300

    feat: new modal

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,6 +1,6 @@
-import { HTMLAttributes, PropsWithChildren } from 'react'
+import { Fragment, HTMLAttributes, PropsWithChildren } from 'react'
 
-import { Dialog } from '@headlessui/react'
+import { Dialog, Transition } from '@headlessui/react'
 import { twMerge } from 'tailwind-merge'
 
 import { CloseIcon } from '@/assets/icons'
@@ -12,18 +12,58 @@ type ModalProps = PropsWithChildren<{
 
 export function Modal({ isOpen = false, onClose, children }: ModalProps) {
   return (
-    <Dialog
-      open={isOpen}
-      onClose={onClose}
-      className="relative w-96 rounded-md border bg-white p-5"
-    >
-      <button className="absolute right-2 top-2" onClick={onClose}>
-        <CloseIcon />
-      </button>
+    <Transition.Root show={isOpen} as={Fragment}>
+      <Dialog as="div" className="relative z-10" onClose={onClose}>
+        <Transition.Child
+          as={Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <div className="fixed inset-0 bg-secondary-700 bg-opacity-75 transition-opacity" />
+        </Transition.Child>
 
-      <Dialog.Panel className="relative min-h-[10rem]">{children}</Dialog.Panel>
-    </Dialog>
+        <div className="fixed inset-0 z-10 overflow-y-auto">
+          <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              enterTo="opacity-100 translate-y-0 sm:scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+              leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+            >
+              <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white p-4 text-left shadow-xl transition-all sm:my-8 sm:w-[95%] sm:max-w-lg xl:max-w-[29rem]">
+                <button className="absolute right-4 top-4" onClick={onClose}>
+                  <CloseIcon />
+                </button>
+
+                <div className="py-2">{children}</div>
+              </Dialog.Panel>
+            </Transition.Child>
+          </div>
+        </div>
+      </Dialog>
+    </Transition.Root>
   )
+
+  // return (
+  //   <Dialog
+  //     open={isOpen}
+  //     onClose={onClose}
+  //     className="relative z-30 w-96 rounded-md border bg-white p-5"
+  //   >
+  //     <button className="absolute right-2 top-2" onClick={onClose}>
+  //       <CloseIcon />
+  //     </button>
+
+  //     <Dialog.Panel className="relative min-h-[10rem]">{children}</Dialog.Panel>
+  //   </Dialog>
+  // )
 }
 
 function ModalTitle({ children, className = '', ...props }: HTMLAttributes<HTMLElement>) {
