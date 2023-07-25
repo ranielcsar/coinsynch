10c4684c4f2638f2c27375e94868b6b0853d2e11
ranoob <whyfck@gmail.com>
2023-07-25 19:51:53 -0300

    changes: move chevron icon due to global use

--- src/components/Select/index.tsx
+++ src/components/Select/index.tsx
@@ -3,6 +3,8 @@ import { Fragment, useState } from 'react'
 import { Listbox, Transition } from '@headlessui/react'
 import Image from 'next/image'
 
+import { ChevronDownIcon } from '@/assets/icons'
+
 const people = [
   { name: 'Wade Cooper', image: 'https://picsum.photos/100?random=1' },
   { name: 'Arlene Mccoy', image: 'https://picsum.photos/100?random=2' },
@@ -77,24 +79,5 @@ function Option({ image, name, selected }: OptionProps) {
       />
       {name}
     </span>
-  )
-}
-
-function ChevronDownIcon() {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="16"
-      height="16"
-      strokeWidth={2}
-      stroke="currentColor"
-      className="bi bi-chevron-down"
-      viewBox="0 0 16 16"
-    >
-      <path
-        fillRule="evenodd"
-        d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"
-      />
-    </svg>
   )
 }
