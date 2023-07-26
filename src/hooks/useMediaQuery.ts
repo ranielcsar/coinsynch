bd01f0c0417cb3b92dfb305700db10e642a7ff82
ranoob <whyfck@gmail.com>
2023-07-26 18:40:36 -0300

    fix hydratation error

--- src/hooks/useMediaQuery.ts
+++ src/hooks/useMediaQuery.ts
@@ -1,42 +1,21 @@
 import { useEffect, useState } from 'react'
 
-export function useMediaQuery(query: string): boolean {
-  const getMatches = (query: string): boolean => {
-    // Prevents SSR issues
-    if (typeof window !== 'undefined') {
-      return window.matchMedia(query).matches
-    }
-    return false
-  }
-
-  const [matches, setMatches] = useState<boolean>(getMatches(query))
-
-  function handleChange() {
-    setMatches(getMatches(query))
-  }
+/**
+ * Modified from link below
+ * @see https://observablehq.com/@werehamster/avoiding-hydration-mismatch-when-using-react-hooks
+ * @param mediaQueryString
+ * @returns {unknown}
+ */
+export function useMediaQuery(mediaQueryString: string) {
+  const [matches, setMatches] = useState<boolean>()
 
   useEffect(() => {
-    const matchMedia = window.matchMedia(query)
-
-    // Triggered at the first client-side load and if query changes
-    handleChange()
-
-    // Listen matchMedia
-    if (matchMedia.addListener) {
-      matchMedia.addListener(handleChange)
-    } else {
-      matchMedia.addEventListener('change', handleChange)
-    }
-
-    return () => {
-      if (matchMedia.removeListener) {
-        matchMedia.removeListener(handleChange)
-      } else {
-        matchMedia.removeEventListener('change', handleChange)
-      }
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [query])
+    const mediaQueryList = window.matchMedia(mediaQueryString)
+    const listener = () => setMatches(!!mediaQueryList.matches)
+    listener()
+    mediaQueryList.addEventListener('change', listener)
+    return () => mediaQueryList.removeEventListener('change', listener)
+  }, [mediaQueryString])
 
   return matches
 }
