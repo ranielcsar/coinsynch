d51ff71e101570afc20d2c86c20d3e3f0c533259
ranoob <whyfck@gmail.com>
2023-07-26 18:42:31 -0300

    changes due to new useModal hook and renamed sources

--- src/pages/landing/components/Header/DesktopHeader.tsx
+++ src/pages/landing/components/Header/DesktopHeader.tsx
@@ -1,17 +1,15 @@
-import { useContext } from 'react'
+import { CoinSynchLogo } from '@/assets/icons'
+import { Button, CryptosCarousel } from '@/components'
 
-import { CoinSyncLogo } from '@/assets/icons'
-import { Button, CoinCarousel } from '@/components'
-
-import { landingContext } from '../../context'
+import { useModal } from '../../hooks/useModal'
 
 export function DesktopHeader() {
-  const { setSignUpModalOpen, setSignInModalOpen } = useContext(landingContext)
+  const { openSignIn, openSignUp } = useModal()
 
   return (
     <header className="pt-5 shadow-md">
-      <section className="m-auto grid max-w-[90rem] grid-cols-[1fr,1fr,1fr,25rem,1.2fr] items-center gap-10 px-16 pb-5">
-        <CoinSyncLogo />
+      <section className="m-auto grid max-w-[90rem] grid-cols-[1fr,max-content,1fr,25rem,1.2fr] items-center gap-10 px-16 pb-5">
+        <CoinSynchLogo />
 
         <div className="col-[2] flex gap-5">
           <a href="#about">About us</a>
@@ -19,12 +17,12 @@ export function DesktopHeader() {
         </div>
 
         <div className="col-[4]">
-          <CoinCarousel />
+          <CryptosCarousel />
         </div>
 
         <div className="col-[5] flex justify-end gap-5">
-          <button onClick={() => setSignInModalOpen(true)}>Sign in</button>
-          <Button onClick={() => setSignUpModalOpen(true)}>Sing up</Button>
+          <button onClick={openSignIn}>Sign in</button>
+          <Button onClick={openSignUp}>Sing up</Button>
         </div>
       </section>
     </header>
