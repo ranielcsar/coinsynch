4953b175de179e9743d43fc26eda2bb0ecbf6f25
ranoob <whyfck@gmail.com>
2023-07-26 18:47:04 -0300

    invoice header order

--- src/pages/landing/components/Header/index.tsx
+++ src/pages/landing/components/Header/index.tsx
@@ -5,16 +5,16 @@ import { MobileHeader } from './MobileHeader'
 import { TabletHeader } from './TabletHeader'
 
 export function Header() {
-  const isDesktop = useMediaQuery('(min-width: 1280px)')
-  const isTablet = useMediaQuery('(min-width: 768px)')
+  const isMobile = useMediaQuery('(min-width: 320px) and (max-width: 760px)')
+  const isTablet = useMediaQuery('(min-width: 768px) and (max-width: 1280px)')
 
-  if (isDesktop) {
-    return <DesktopHeader />
+  if (isMobile) {
+    return <MobileHeader />
   }
 
   if (isTablet) {
     return <TabletHeader />
   }
 
-  return <MobileHeader />
+  return <DesktopHeader />
 }
