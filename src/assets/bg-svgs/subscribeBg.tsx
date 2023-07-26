4d86b1203142db35d388a043361ebb18c542d418
ranoob <whyfck@gmail.com>
2023-07-26 18:38:49 -0300

    assets: subscribe bg

---
+++ src/assets/bg-svgs/subscribeBg.tsx
@@ -0,0 +1,59 @@
+export function SubscribeBg() {
+  return (
+    <svg
+      width="100%"
+      height="432"
+      viewBox="0 0 320 432"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M-702.578 432H600.604C671.483 387.703 766.329 240.912 658.701 201.757C524.166 152.813 444.709 224.06 237.49 250.701C30.27 277.341 94.8286 208.882 -122.323 113.163C-296.044 36.5875 -576.17 35.0891 -702.578 57.7023V432Z"
+        fill="url(#paint0_linear_1_3794)"
+      />
+      <path
+        d="M-615.576 518.976L541.393 570.112C592.567 502.517 662.597 273.757 558.238 229.021C427.789 173.1 375.829 278.775 200.456 333.937C25.0835 389.1 64.6532 288.601 -150.091 175.186C-321.886 84.4537 -524.317 222.412 -629.705 264.873L-615.576 518.976Z"
+        fill="url(#paint1_linear_1_3794)"
+      />
+      <path
+        d="M-658.641 429.715L597.207 433.962C651.572 384.014 620.52 158.034 518.982 128.896C392.06 92.4734 334.967 169.765 157.675 214.315C-19.6176 258.865 24.8843 185.637 -182.963 110.355C-349.242 50.1283 -604.845 101.977 -711.862 135.43L-658.641 429.715Z"
+        fill="url(#paint2_linear_1_3794)"
+      />
+      <defs>
+        <linearGradient
+          id="paint0_linear_1_3794"
+          x1="-7.99576"
+          y1="62.0245"
+          x2="-7.99576"
+          y2="383.592"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#FBAB34" stopOpacity="0.2" />
+          <stop offset="1" stopColor="#FBAB34" stopOpacity="0" />
+        </linearGradient>
+        <linearGradient
+          id="paint1_linear_1_3794"
+          x1="-62.4605"
+          y1="94.2848"
+          x2="-17.4113"
+          y2="543.18"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#FBAB34" stopOpacity="0.2" />
+          <stop offset="1" stopColor="#FBAB34" stopOpacity="0" />
+        </linearGradient>
+        <linearGradient
+          id="paint2_linear_1_3794"
+          x1="-91.6474"
+          y1="49.8263"
+          x2="-59.557"
+          y2="369.593"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#FBAB34" stopOpacity="0.2" />
+          <stop offset="1" stopColor="#FBAB34" stopOpacity="0" />
+        </linearGradient>
+      </defs>
+    </svg>
+  )
+}
