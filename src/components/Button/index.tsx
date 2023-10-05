7d1869efefd3aac36e66b9b2ac17d049f2115411
ranoob <whyfck@gmail.com>
2023-10-05 16:30:51 -0300

    fix: added type="button" on buttons

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -9,6 +9,7 @@ export function Button({
 
   return (
     <button
+      type="button"
       {...props}
       className={twMerge(
         'inline-flex max-h-[2.25rem] w-max items-center justify-center gap-2 rounded-[32px] bg-primary-500 px-6 py-2 text-white disabled:bg-primary-300 md:py-6 md:text-base',
